2583ac6decb09d2967a8a18e60ff87fd87ba13c4
ssimm333 <stanislavsimanovich1@gmail.com>
2026-02-04 14:42:32 +0000

    Initial Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portfolio loaded');
+    
+    // Smooth scrolling for anchor links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
